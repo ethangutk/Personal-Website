9fe0aef7c99a2de5f85573cd09a46fdb91f1d431
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-24 11:43:35 -0400

    Add files via upload

---
+++ PersonalWebsiteTemplate/mainPage.js
@@ -0,0 +1,15 @@
+/*
+    Your Name's Personal Website
+    Copyright 2022 (C) Your Name
+
+    CSS File
+    07/24/2022
+*/
+
+/*Prepare the page for start up*/
+function startUpPage() {
+    console.log("Page loaded.")
+}
+
+/*Fire off functions*/
+window.onload = startUpPage;
